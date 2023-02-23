fad432cecc68672f41f1ac1b895700d566179acd
sibbe23 <sibbe23@gmail.com>
2023-02-23 13:38:30 +0530

    Saved

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,7 @@ exports.getProducts = (req, res, next) => {
   .catch(err=>console.log(err))
 };
 
+
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findByPk(prodId)
